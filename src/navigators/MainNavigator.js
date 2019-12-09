f6d5365df312c7a019d2dd445d31303a4cbe50b6
Manoel Carvalho <manoelmecat@gmail.com>
2019-12-09 08:41:02 -0300

    Redux

--- src/navigators/MainNavigator.js
+++ src/navigators/MainNavigator.js
@@ -1,14 +1,30 @@
-import {createStackNavigator} from 'react-navigation-stack';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+
 import Home from '../screens/Home';
 import Login from '../screens/Login';
 
-const MainNavigator = createStackNavigator({
-  Login: {
-    screen: Login,
+const MainNavigator = createBottomTabNavigator(
+  {
+    Login: {
+      screen: Login,
+    },
+    Home: {
+      screen: Home,
+    },
   },
-  Home: {
-    screen: Home,
+  {
+    tabBarOptions: {
+      activeTintColor: '#fff',
+      inactiveTintColor: '#a35029',
+      labelStyle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+      },
+      style: {
+        backgroundColor: 'tomato',
+      },
+    },
   },
-});
+);
 
 export default MainNavigator;
